4cfe506d5be5b73fcef1c141b56c0c3a68a5e460
Mark Harrison <mark@mivok.net>
2017-04-26 14:27:08 -0400

    Add zoom detect script

---
+++ helpers/zoom_detect.lua
@@ -0,0 +1,37 @@
+-- Hammerspoon script to automatically update your slack status when in zoom
+--
+-- To use:
+--
+-- * Install and set up the slack_status.sh script (make sure it's in your
+--   path)
+-- * Ensure there is a 'zoom' preset (one is created by default during setup)
+-- * Install hammerspoon (brew cask install hammerspoon) if you don't have it
+--   already.
+-- * Copy this file to ~/.hammerspoon
+-- * Add the following line to ~/.hammerspoon/init.lua
+--      local zoom_detect = require("zoom_detect")
+
+-- Configuration
+check_interval=20 -- How often to check if you're in zoom, in seconds
+
+function update_status(status)
+    task = hs.execute("slack_status.sh " .. status, true)
+end
+
+inzoom = false
+timer = hs.timer.doEvery(check_interval, function()
+    if hs.application.find("Zoom Meeting ID") ~= nil then
+        if inzoom == false then
+            inzoom = true
+            hs.notify.show("Started zoom meeting", "Updating slack status", "")
+            update_status("zoom")
+        end
+    else
+        if inzoom == true then
+            inzoom = false
+            hs.notify.show("Left zoom meeting", "Updating slack status", "")
+            update_status("none")
+        end
+    end
+end)
+timer:start()
